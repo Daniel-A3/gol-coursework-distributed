620958a9231524092bbc9cb3ee83941417201a16
MykolaTakun <takun1812@gmail.com>
2024-11-08 20:17:24 +0000

    works with workers for 1 server

--- gol/server/server/server.go
+++ gol/server/server/server.go
@@ -1,20 +1,19 @@
 package main
 
 import (
-	//	"errors"
 	"flag"
 	"fmt"
 	"net"
+<<<<<<< HEAD
+=======
+	"net/rpc"
+>>>>>>> 1dd03dd (works with workers for 1 server)
 	"sync"
 	"uk.ac.bris.cs/gameoflife/gol/server/stubs"
 	"uk.ac.bris.cs/gameoflife/util"
-	//	"time"
-	//	"math/rand"
-	"net/rpc"
 )
 
-type GOL struct {
-}
+type GOL struct{}
 
 var mu sync.Mutex
 
@@ -122,15 +121,84 @@ func calculateNextState(p stubs.Params, world [][]byte, startX, endX, startY, en
 	height := endY - startY
 	width := endX - startX
 	nextWorld := createWorld(height, width)
+	var cF []util.Cell
+
+	numWorkers := height
+	if numWorkers > 16 {
+		numWorkers = 16
+	}
+	if numWorkers < 4 {
+		numWorkers = 4
+	}
+
+	rowsPerWorker := height / numWorkers
+	extraRows := height % numWorkers
+
+	var wg sync.WaitGroup
+	flippedCells := make([][]util.Cell, numWorkers) // Slice to store flipped cells from each worker
+	worlds := make([][][]byte, numWorkers)          // Slice to store partial nextWorlds from each worker
+
+	for w := 0; w < numWorkers; w++ {
+		startY := w * rowsPerWorker
+		endY := startY + rowsPerWorker
+		if w < extraRows {
+			endY++
+		}
+
+		wg.Add(1)
+		go func(workerIndex, startY, endY int) {
+			defer wg.Done()
+			flipped, partialWorld := calculateNextState(req.P, req.World, req.StartX, req.EndX, startY, endY)
+			flippedCells[workerIndex] = flipped
+			worlds[workerIndex] = partialWorld
+		}(w, startY, endY)
+	}
+
+	wg.Wait()
+
+	// Combine results from workers
+	for _, flipped := range flippedCells {
+		cF = append(cF, flipped...)
+	}
+
+	// Merge `worlds` back into `nextWorld`
+	startY := 0
+	for _, partialWorld := range worlds {
+		for i := range partialWorld {
+			copy(nextWorld[startY+i], partialWorld[i])
+		}
+		startY += len(partialWorld)
+	}
+
+	res.FlippedCells = cF
+	res.World = nextWorld
+	return nil
+}
+
+func calculateNextState(p stubs.Params, world [][]byte, startX, endX, startY, endY int) ([]util.Cell, [][]byte) {
+	height := endY - startY
+	width := endX - startX
+	nextWorld := createWorld(height, width)
 	var cellsFlipped []util.Cell
 
 	countAlive := func(y, x int) int {
 		alive := 0
+<<<<<<< HEAD
 		for i := -1; i < 2; i++ {
 			for j := -1; j < 2; j++ {
 				neighbourY := (y + i + p.ImageHeight) % p.ImageHeight
 				neighbourX := (x + j + p.ImageWidth) % p.ImageWidth
 				if !(i == 0 && j == 0) && (world[neighbourY][neighbourX] == 255) {
+=======
+		for i := -1; i <= 1; i++ {
+			for j := -1; j <= 1; j++ {
+				if i == 0 && j == 0 {
+					continue // Skip the cell itself
+				}
+				neighbourY := (y + i + p.ImageHeight) % p.ImageHeight
+				neighbourX := (x + j + p.ImageWidth) % p.ImageWidth
+				if world[neighbourY][neighbourX] == 255 {
+>>>>>>> 1dd03dd (works with workers for 1 server)
 					alive++
 				}
 			}
@@ -141,7 +209,10 @@ func calculateNextState(p stubs.Params, world [][]byte, startX, endX, startY, en
 	for y := startY; y < endY; y++ {
 		for x := startX; x < endX; x++ {
 			aliveNeighbour := countAlive(y, x)
+<<<<<<< HEAD
 
+=======
+>>>>>>> 1dd03dd (works with workers for 1 server)
 			if world[y][x] == 255 {
 				if aliveNeighbour < 2 || aliveNeighbour > 3 {
 					nextWorld[y-startY][x] = 0
@@ -165,9 +236,15 @@ func calculateNextState(p stubs.Params, world [][]byte, startX, endX, startY, en
 func calculateAliveCells(p stubs.Params, world [][]byte, startX, endX, startY, endY int) (int, []util.Cell) {
 	var flipped []util.Cell
 	count := 0
+<<<<<<< HEAD
 	for y := 0; y < endY; y++ {
 		for x := 0; x < endX; x++ {
 			if world[y][x] == 255 {
+=======
+	for y := req.StartY; y < req.EndY; y++ {
+		for x := req.StartX; x < req.EndX; x++ {
+			if req.World[y][x] == 255 {
+>>>>>>> 1dd03dd (works with workers for 1 server)
 				count++
 				flipped = append(flipped, util.Cell{X: x, Y: y})
 			}
@@ -204,6 +281,7 @@ func (gol *GOL) ClosingSystem(req stubs.Request, response *stubs.Response) error
 	close(closingServer)
 	return nil
 }
+
 func main() {
 	pAddr := flag.String("port", "8030", "Port to listen on")
 	flag.Parse()
@@ -215,14 +293,11 @@ func main() {
 		listener.Close()
 	}(listener)
 	for {
-		// Accept connections until the listener is closed
 		conn, err := listener.Accept()
 		if err != nil {
-			// Break out of the loop on listener close or other errors
 			fmt.Println("Closing server...")
 			break
 		}
 		rpc.ServeConn(conn)
 	}
-
 }
